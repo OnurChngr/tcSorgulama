c522067767c78197f00f8cd2b04db17457177a6a
onurc <onurc@LAPTOP-7342543N>
2021-10-27 18:14:24 +0300

    Add project files.

---
+++ tcSorgulama/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace tcSorgulama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTc = new System.Windows.Forms.TextBox();
+            this.btnKontrol = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tc Kimliğinizi Giriniz:";
+            // 
+            // txtTc
+            // 
+            this.txtTc.Location = new System.Drawing.Point(236, 164);
+            this.txtTc.Name = "txtTc";
+            this.txtTc.Size = new System.Drawing.Size(100, 22);
+            this.txtTc.TabIndex = 1;
+            // 
+            // btnKontrol
+            // 
+            this.btnKontrol.Location = new System.Drawing.Point(236, 246);
+            this.btnKontrol.Name = "btnKontrol";
+            this.btnKontrol.Size = new System.Drawing.Size(100, 35);
+            this.btnKontrol.TabIndex = 2;
+            this.btnKontrol.Text = "Kontrol Et";
+            this.btnKontrol.UseVisualStyleBackColor = true;
+            this.btnKontrol.Click += new System.EventHandler(this.btnKontrol_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(198, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Uygun Bir Tc Numarası Giriniz";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnKontrol);
+            this.Controls.Add(this.txtTc);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTc;
+        private System.Windows.Forms.Button btnKontrol;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ tcSorgulama/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tcSorgulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        int[] tc = new int[11];
+
+
+        private void btnKontrol_Click(object sender, EventArgs e)
+        {
+            string tckimlikno = txtTc.Text;
+
+            if (txtTc.Text != " ")
+            {
+                if (tckimlikno.Length == 11)
+                {
+                    if (long.TryParse(tckimlikno, out long result))
+                    {
+                        for (int i = 0; i < tc.Length; i++)
+                        {
+
+                            tc[i] = int.Parse(tckimlikno.Substring(i, 1));
+                        }
+
+                        if (tc[0] != 0)
+                        {
+
+                            int a = ((tc[0] + tc[2] + tc[4] + tc[6] + tc[8]) * 7) + ((tc[1] + tc[3] + tc[5] + tc[7]) * 9);
+
+                            if (a % 10 == tc[9])
+                            {
+                                int toplam = tc[0] + tc[2] + tc[4] + tc[6] + tc[8] + tc[1] + tc[3] + tc[5] + tc[7] + tc[9];
+
+                                if (toplam % 10 == tc[10])
+                                {
+                                    int toplam2 = tc[0] + tc[2] + tc[4] + tc[6] + tc[8];
+
+                                    if ((toplam2 * 8) % 10 == tc[10])
+                                    {
+                                        MessageBox.Show("Geçerli Tc");
+                                    }
+                                    else
+                                    {
+                                        MessageBox.Show("6.maddeye uymuyor!");
+                                    }
+                                }
+                                else
+                                {
+
+                                    MessageBox.Show("5.maddeye uymuyor!");
+                                }
+
+                            }
+                            else
+                            {
+                                MessageBox.Show("4.maddeye uymuyor!");
+                            }
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("Tc no sıfırla başlayamaz!");
+
+                        }
+
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Girilen değer doğru formatta değil!");
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Girilen değer 11 karakter olmalıdır!");
+                }
+            }
+
+
+        }
+    }
+}
